69e4f2b6e5a385e21b629513293e9273683e26b5
Sumit Singh <sumit.singh@definelabs.com>
2024-08-05 16:42:16 +0530

    update vertical page adapter

--- app/src/main/java/com/royal/edunotes/_adapters/VerticlePagerAdapter.java
+++ app/src/main/java/com/royal/edunotes/_adapters/VerticlePagerAdapter.java
@@ -2,10 +2,8 @@ package com.royal.edunotes._adapters;
 
 import android.content.Context;
 import androidx.viewpager.widget.PagerAdapter;
-
-import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +25,7 @@ import java.util.ArrayList;
 
 public class VerticlePagerAdapter extends PagerAdapter {
 
+    private static final String TAG = "VerticlePagerAdapter";
     Context mContext;
     LayoutInflater mLayoutInflater;
     ArrayList<QuoteModel> quoteModels;
@@ -36,12 +35,13 @@ public class VerticlePagerAdapter extends PagerAdapter {
 
     ArrayList<ModelDatabase> modelDatabases;
 
-    public VerticlePagerAdapter(Context context, ArrayList<QuoteModel> quoteModels, ClickInterface clickInterface,ArrayList<ModelDatabase> modelDatabases) {
+    public VerticlePagerAdapter(Context context, ArrayList<QuoteModel> quoteModels, ClickInterface clickInterface, ArrayList<ModelDatabase> modelDatabases) {
         mContext = context;
         mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.quoteModels = quoteModels;
         this.clickInterface = clickInterface;
         this.modelDatabases = modelDatabases;
+        Log.d(TAG, "Adapter instantiated");
     }
 
     @Override
@@ -58,110 +58,94 @@ public class VerticlePagerAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, final int position) {
         View itemView = mLayoutInflater.inflate(R.layout.content_main, container, false);
 
-        TextView titleTxt = (TextView) itemView.findViewById(R.id.title);
-        TextView hackTxt = (TextView) itemView.findViewById(R.id.hackText);
-        ImageView hackTxt2 = (ImageView) itemView.findViewById(R.id.hackText2);
+        TextView titleTxt = itemView.findViewById(R.id.title);
+        TextView hackTxt = itemView.findViewById(R.id.hackText);
+        ImageView hackTxt2 = itemView.findViewById(R.id.hackText2);
 
+        LinearLayout copy = itemView.findViewById(R.id.copyLL);
+        final LinearLayout starLL = itemView.findViewById(R.id.starLL);
+        LinearLayout share = itemView.findViewById(R.id.shareLL);
+        LinearLayout otherapp = itemView.findViewById(R.id.ourappLL);
 
-        LinearLayout copy = (LinearLayout) itemView.findViewById(R.id.copyLL);
-        final LinearLayout starLL = (LinearLayout) itemView.findViewById(R.id.starLL);
-        LinearLayout share = (LinearLayout) itemView.findViewById(R.id.shareLL);
-        LinearLayout otherapp = (LinearLayout) itemView.findViewById(R.id.ourappLL);
+        final ImageView star = itemView.findViewById(R.id.star);
 
-        final ImageView star = (ImageView) itemView.findViewById(R.id.star);
-
-        hackTxt.setText(quoteModels.get(position).getQuote());
- //       titleTxt.setText(quoteModels.get(position).getCategoryName());
-
-        String url = quoteModels.get(position).getValue();
-
-        if (url != null) {
-            hackTxt2.setVisibility(View.VISIBLE);
-            Picasso.get().load(url).into(hackTxt2);
-        } else {
-            hackTxt2.setVisibility(View.GONE);
-
+        QuoteModel currentQuote = quoteModels.get(position);
+        if (currentQuote == null) {
+            Log.e(TAG, "instantiateItem: currentQuote is null at position " + position);
+            return itemView;
         }
 
-        int[] colors = {Color.rgb(36, 7, 80),Color.rgb(255, 0, 128), Color.rgb(50, 1, 47), Color.rgb(249, 115, 0), Color.rgb(27, 66, 66), Color.rgb(64, 165, 120), Color.rgb(100, 13, 107), Color.rgb(181, 27, 117), Color.rgb(0, 0, 0)};
+        hackTxt.setText(currentQuote.getQuote());
+        String url = currentQuote.getValue();
+
+        if (url != null && !url.isEmpty()) {
+            hackTxt2.setVisibility(View.VISIBLE);
+            Picasso.get().load(url).into(hackTxt2);
+            Log.d(TAG, "instantiateItem: Image loaded for position " + position);
+        } else {
+            hackTxt2.setVisibility(View.GONE);
+            Log.d(TAG, "instantiateItem: No image for position " + position);
+        }
+
+        int[] colors = {Color.rgb(36, 7, 80), Color.rgb(255, 0, 128), Color.rgb(50, 1, 47), Color.rgb(249, 115, 0), Color.rgb(27, 66, 66), Color.rgb(64, 165, 120), Color.rgb(100, 13, 107), Color.rgb(181, 27, 117), Color.rgb(0, 0, 0)};
         int randomIndex = (int) (Math.random() * colors.length);
         int randomColor = colors[randomIndex];
 
-
-        //       titleTxt.setText(quoteModels.get(position).getCategoryName());
-
-
-
-// Set the text color of the TextView to the random color
         hackTxt.setTextColor(randomColor);
 
-        for(int i =0;i<modelDatabases.size();i++){
-
-            if(quoteModels.get(position).getQuote().equals(modelDatabases.get(i).getNote())){
-                quoteModels.get(position).setBookmark("1");
-                quoteModels.get(position).setBookmared(true);
-                break;
-            }else{
-
-                quoteModels.get(position).setBookmark("0");
-                quoteModels.get(position).setBookmared(false);
-
+        if (modelDatabases != null) {
+            for (ModelDatabase modelDatabase : modelDatabases) {
+                if (modelDatabase != null && currentQuote.getQuote() != null && currentQuote.getQuote().equals(modelDatabase.getNote())) {
+                    currentQuote.setBookmark("1");
+                    currentQuote.setBookmared(true);
+                    Log.d(TAG, "instantiateItem: Quote bookmarked at position " + position);
+                    break;
+                } else {
+                    currentQuote.setBookmark("0");
+                    currentQuote.setBookmared(false);
+                }
             }
 
-        }
-
-        for(int i =0;i<modelDatabases.size();i++){
-
-            if(quoteModels.get(position).getValue().equals(modelDatabases.get(i).getNote())){
-                quoteModels.get(position).setBookmark("1");
-                quoteModels.get(position).setBookmared(true);
-                break;
-            }else{
-
-                quoteModels.get(position).setBookmark("0");
-                quoteModels.get(position).setBookmared(false);
-
+            for (ModelDatabase modelDatabase : modelDatabases) {
+                if (modelDatabase != null && currentQuote.getValue() != null && currentQuote.getValue().equals(modelDatabase.getNote())) {
+                    currentQuote.setBookmark("1");
+                    currentQuote.setBookmared(true);
+                    Log.d(TAG, "instantiateItem: Quote value bookmarked at position " + position);
+                    break;
+                } else {
+                    currentQuote.setBookmark("0");
+                    currentQuote.setBookmared(false);
+                }
             }
-
+        } else {
+            Log.e(TAG, "instantiateItem: modelDatabases is null");
         }
 
-        if (quoteModels.get(position).isBookmared()) {
+        if (currentQuote.isBookmared()) {
             star.setImageResource(R.drawable.starfilled);
         } else {
             star.setImageResource(R.drawable.star);
         }
 
-
-        copy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clickInterface.onCopyClick(quoteModels.get(position));
-            }
+        copy.setOnClickListener(view -> {
+            clickInterface.onCopyClick(currentQuote);
         });
 
-
-        starLL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clickInterface.onBoookmarkClick(quoteModels.get(position), star);
-            }
+        starLL.setOnClickListener(view -> {
+            clickInterface.onBoookmarkClick(currentQuote, star);
         });
 
-        share.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clickInterface.onShareClick(quoteModels.get(position));
-            }
+        share.setOnClickListener(view -> {
+            clickInterface.onShareClick(currentQuote);
         });
 
-        otherapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clickInterface.onMoreAppsClick();
-            }
+        otherapp.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: Other apps clicked at position " + position);
+            clickInterface.onMoreAppsClick();
         });
 
         container.addView(itemView);
+        Log.d(TAG, "instantiateItem: itemView added for position " + position);
 
         return itemView;
     }
@@ -171,8 +155,7 @@ public class VerticlePagerAdapter extends PagerAdapter {
 
         void onCopyClick(QuoteModel CategoryModel);
 
-        void
-        onShareClick(QuoteModel CategoryModel);
+        void onShareClick(QuoteModel CategoryModel);
 
         void onMoreAppsClick();
     }
@@ -180,5 +163,6 @@ public class VerticlePagerAdapter extends PagerAdapter {
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((LinearLayout) object);
+        Log.d(TAG, "destroyItem: itemView removed for position " + position);
     }
 }
